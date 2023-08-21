75e122b9235d99e795391bba3c8cf06afb2958a0
Valandria <valkyriereaver@gmail.com>
2023-08-21 14:41:01 -0500

    Initial Build
    Initial build of the blank template repository creator for callout packs.

---
+++ Repo Template.net/Repo Template/CalloutName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using FivePD.API;
+using FivePD.API.Utils;
+
+namespace Repo_Template
+{
+    [CalloutProperties("CalloutName", "Creator", "0.0.0")]
+    public class CalloutName : Callout
+    {
+    }
+}
